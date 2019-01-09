1be3d73904fe1f36cd1867f6d8d0e4be23d1c7cc
Tobechukwu Obitube <riseagain47@gmail.com>
2019-01-09 15:54:54 +0100

    Added index.js package-lock.json and package.json server files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  return res.status(200).send('Welcome to Qestioner App');
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+});
