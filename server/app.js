80cda95376b5707a457ea19e4807c81a7d7542d5
tobechukwuobitube <tobechukwuobitube@gmail.com>
2019-01-13 23:15:00 +0100

    Modified app.js

--- server/app.js
+++ server/app.js
@@ -16,8 +16,9 @@ app.use(userRoutes);
 
 app.get("/", (req, res) => { return res.json('Start with /users files') });
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
 
 process.on('exit', () => server.close())
